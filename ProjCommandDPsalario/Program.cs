b5985e8e496f779e255e3ddc31a69c1c4587ec8c
helofahl@gmail.com <helofahl@gmail.com>
2022-10-06 00:42:35 -0300

    Insercao de informacao digitada

--- ProjCommandDPsalario/Program.cs
+++ ProjCommandDPsalario/Program.cs
@@ -6,14 +6,21 @@ namespace ProjCommandDPsalario
     {
         static void Main(string[] args) //client 
         {
-            
+            //parâmetros fixos
             var receiver = new ReceiverSalario();
             CommandSalario command = new CommandSalario(receiver,"Heloísa", 1200,200);
             Invoke invoke = new Invoke();
             ExecutaInvoke(receiver, command, invoke);
 
+          //parâmetros digitados
             receiver = new ReceiverSalario();
-            command = new CommandSalario(receiver, "Roberta", 1200, 800);
+            Console.Write("Nome: ");
+            string n= Console.ReadLine();
+            Console.Write("Salário Atual: ");
+            float s = float.Parse(Console.ReadLine());
+            Console.Write("Valor reajuste: ");
+            float v = float.Parse(Console.ReadLine());  
+            command = new CommandSalario(receiver, n,s, v);
             invoke = new Invoke();
             ExecutaInvoke(receiver, command, invoke);
         }
